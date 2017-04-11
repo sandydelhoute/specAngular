57c7a4337e23d67d47637808ea52c9d970639209
Roman Robert (Voxran) <r.voxran@gmail.com>
2017-04-11 16:47:23 +0200

    amelioration css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,115 +1,116 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './layout-partie.html'
+	selector: 'app-root',
+	templateUrl: './layout-partie.html', 
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
 
-  role = {
-    roleName : 'Ancien',
-    carte : 'ancien.JPG',
-  };
+	role = {
+		roleName : 'Ancien',
+		carte : 'ancien.JPG',
+	};
 
-  player = {
-    name : 'Ancien',
-    role : 'ancien.JPG',
-  };
+	player = {
+		name : 'Ancien',
+		role : 'ancien.JPG',
+	};
 
-  listPlayers = [
-  	{ 
-  		name : 'Sandy',
-  		role : 'loup'
-  	},
-  	{ 
-  		name : 'Roman',
-  		role : 'ancien'
-  	},
-  ];
+	listPlayers = [
+		{ 
+			name : 'Sandy',
+			role : 'loup'
+		},
+		{ 
+			name : 'Roman',
+			role : 'ancien'
+		},
+	];
 
-  roles = [
-    {
-      roleName : 'Ancien',
-      carte : 'ancien.JPG',
-      visible : true,
-    },
-    {
-      roleName : 'Bouc Emissaire',
-      carte : 'boucEmissaire.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Chasseur',
-      carte : 'chasseur.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Cupidon',
-      carte : 'cupidon.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Idiot',
-      carte : 'idiot.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Joueur de Flûte',
-      carte : 'joueurFlute.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Loup Garou',
-      carte : 'loupGarou.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Maire',
-      carte : 'maire.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Petit Fille',
-      carte : 'petiteFille.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Salvateur',
-      carte : 'salvateur.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Sorcière',
-      carte : 'sorciere.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Villageois',
-      carte : 'villageois.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Voleur',
-      carte : 'voleur.jpg',
-      visible : true,
-    },
-    {
-      roleName : 'Voyante',
-      carte : 'voyante.jpg',
-      visible : true,
-    },
-  ];
+	roles = [
+		{
+			roleName : 'Ancien',
+			carte : 'ancien.JPG',
+			visible : true,
+		},
+		{
+			roleName : 'Bouc Emissaire',
+			carte : 'boucEmissaire.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Chasseur',
+			carte : 'chasseur.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Cupidon',
+			carte : 'cupidon.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Idiot',
+			carte : 'idiot.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Joueur de Flûte',
+			carte : 'joueurFlute.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Loup Garou',
+			carte : 'loupGarou.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Maire',
+			carte : 'maire.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Petit Fille',
+			carte : 'petiteFille.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Salvateur',
+			carte : 'salvateur.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Sorcière',
+			carte : 'sorciere.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Villageois',
+			carte : 'villageois.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Voleur',
+			carte : 'voleur.jpg',
+			visible : true,
+		},
+		{
+			roleName : 'Voyante',
+			carte : 'voyante.jpg',
+			visible : true,
+		},
+	];
 
-  enterEditName( element: HTMLInputElement ){
-    element.focus();
-  }
+	enterEditName( element: HTMLInputElement ){
+		element.focus();
+	}
 
-  editName( updatedText: string ){
-    this.role.roleName = updatedText;
-  }
+	editName( updatedText: string ){
+		this.role.roleName = updatedText;
+	}
 
-  cancelEdit( element: HTMLInputElement ){
-    element.value = this.role.roleName;
-  }
+	cancelEdit( element: HTMLInputElement ){
+		element.value = this.role.roleName;
+	}
 
 }
--- src/app/tchat/listmessages.component.ts
+++ src/app/tchat/listmessages.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'listMessages',
   templateUrl: './listMessages.html',
+  styleUrls:['./listMessages.css']
 
 })
 export class ListMessages  {
