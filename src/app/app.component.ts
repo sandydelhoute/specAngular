7f569206d2f35094fa8784f1ead71d051811a043
Roman Robert (Voxran) <r.voxran@gmail.com>
2017-04-12 12:09:49 +0200

    Modifs app.component importantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,159 +7,258 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
 
-	role = {
+	private nbPlayers 		= 0;
+	private minPlayers 		= 6;
+	private nbLoups			= 0;
+
+
+	public role = {
 		roleName : 'Bouc Emissaire',
 		carte : '/assets/cartes/boucEmissaire.jpg',
 	};
 
 	player = {
-		name : 'Chasseur',
-		role : this.role,
+		name 	: 'Chasseur',
+		role 	: this.role,
+		isReady : false
 	};
 
-	listPlayers = [
+	public listPlayers = [
 		{ 
-			name : 'Sandy',
-			role : 'loup'
+			name 	: 'Sandy',
+			role 	: null,
+			isReady	: false
 		},
 		{ 
-			name : 'Roman',
-			role : 'ancien'
+			name 	: 'Test',
+			role 	: null,
+			isReady	: false
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{ 
+			name 	: 'Truc',
+			role 	: null,
+			isReady	: false
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{ 
+			name 	: 'Roman',
+			role 	: null,
+			isReady	: false
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{ 
+			name 	: 'Machin',
+			role 	: null,
+			isReady	: false
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{ 
+			name 	: 'Bidule',
+			role 	: null,
+			isReady	: false
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{ 
+			name 	: 'Trucmuche',
+			role 	: null,
+			isReady	: false
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{ 
+			name 	: 'Test2',
+			role 	: null,
+			isReady	: false
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{ 
+			name 	: 'Tamere',
+			role 	: null,
+			isReady	: false
+		}		
+	];
+
+	listRoles = [
+		{
+			roleName 	: 'Loup Garou',
+			carte 		: '/assets/cartes/loupGarou.jpg',
+			visible 	: true,
+			max			: 0.5
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{
+			roleName 	: 'Villageois',
+			carte 		: '/assets/cartes/villageois.jpg',
+			visible 	: true,
+			max			: null
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{
+			roleName 	: 'Ancien',
+			carte 		: '/assets/cartes/ancien.JPG',
+			visible 	: true,
+			max			: 1
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{
+			roleName 	: 'Bouc Emissaire',
+			carte 		: '/assets/cartes/boucEmissaire.jpg',
+			visible 	: true,
+			max			: 1
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{
+			roleName 	: 'Chasseur',
+			carte 		: '/assets/cartes/chasseur.jpg',
+			visible 	: true,
+			max			: 1
 		},
-			{ 
-			name : 'Roman',
-			role : 'ancien'
+		{
+			roleName 	: 'Cupidon',
+			carte 		: '/assets/cartes/cupidon.jpg',
+			visible 	: true,
+			max			: 1
+		},
+		{
+			roleName 	: 'Idiot',
+			carte 		: '/assets/cartes/idiot.jpg',
+			visible 	: true,
+			max			: 1
+		},
+		{
+			roleName 	: 'Joueur de Flûte',
+			carte 		: '/assets/cartes/joueurFlute.jpg',
+			visible 	: true,
+			max			: 1
 		},
 		
-	];
-
-	roles = [
 		{
-			roleName : 'Ancien',
-			carte : '/assets/cartes/ancien.JPG',
-			visible : true,
+			roleName 	: 'Maire',
+			carte 		: '/assets/cartes/maire.jpg',
+			visible 	: true,
+			max			: 1
 		},
 		{
-			roleName : 'Bouc Emissaire',
-			carte : '/assets/cartes/boucEmissaire.jpg',
-			visible : true,
+			roleName 	: 'Petit Fille',
+			carte 		: '/assets/cartes/petiteFille.jpg',
+			visible 	: true,
+			max			: 1
 		},
 		{
-			roleName : 'Chasseur',
-			carte : '/assets/cartes/chasseur.jpg',
-			visible : true,
+			roleName 	: 'Salvateur',
+			carte 		: '/assets/cartes/salvateur.jpg',
+			visible 	: true,
+			max			: 1
 		},
 		{
-			roleName : 'Cupidon',
-			carte : '/assets/cartes/cupidon.jpg',
-			visible : true,
+			roleName 	: 'Sorcière',
+			carte 		: '/assets/cartes/sorciere.jpg',
+			visible 	: true,
+			max			: 1
 		},
 		{
-			roleName : 'Idiot',
-			carte : '/assets/cartes/idiot.jpg',
-			visible : true,
+			roleName 	: 'Voleur',
+			carte 		: '/assets/cartes/voleur.jpg',
+			visible 	: true,
+			max			: 1
 		},
 		{
-			roleName : 'Joueur de Flûte',
-			carte : '/assets/cartes/joueurFlute.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Loup Garou',
-			carte : '/assets/cartes/loupGarou.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Maire',
-			carte : '/assets/cartes/maire.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Petit Fille',
-			carte : '/assets/cartes/petiteFille.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Salvateur',
-			carte : '/assets/cartes/salvateur.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Sorcière',
-			carte : '/assets/cartes/sorciere.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Villageois',
-			carte : '/assets/cartes/villageois.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Voleur',
-			carte : '/assets/cartes/voleur.jpg',
-			visible : true,
-		},
-		{
-			roleName : 'Voyante',
-			carte : '/assets/cartes/voyante.jpg',
-			visible : true,
+			roleName 	: 'Voyante',
+			carte 		: '/assets/cartes/voyante.jpg',
+			visible 	: true,
+			max			: 1
 		},
 	];
 
-	enterEditName( element: HTMLInputElement ){
-		element.focus();
+
+	addPlayer(playername:string, errorMessage:HTMLElement){
+
+		if(typeof playername != 'undefined' && playername != ''){
+
+			errorMessage.classList.add('hidden');
+			var isAvailable = true;
+
+			this.listPlayers.forEach(function(player){
+				if(player.name == playername)
+					isAvailable = false;
+			});
+
+			if(isAvailable == true){
+
+				let newPlayer = {
+					name	: playername,
+					role 	: this.setRandomRole().roleName,
+					isReady : false
+				}
+
+				this.listPlayers.push(newPlayer);
+
+				return newPlayer;
+
+			} else {
+
+				errorMessage.classList.remove('hidden');
+
+			} 
+
+		}
+
 	}
 
-	editName( updatedText: string ){
-		this.role.roleName = updatedText;
+
+	setReady(player){
+
+		player = player.isReady = true;
+
+		this.listPlayers[player] = player;
+
+		var ready = this.areTheyReady();
+
+		if(ready == true)
+			this.launchParty();
+
 	}
 
-	cancelEdit( element: HTMLInputElement ){
-		element.value = this.role.roleName;
+	areTheyReady(){
+		var allAreReady = true;
+
+		this.listPlayers.forEach(function(player){
+			if(player.isReady != true)
+				allAreReady = false;
+		});
+
+		return allAreReady;
+
+	}
+
+	launchParty(){
+
+	}
+
+	setRandomRole(){
+
+		this.nbPlayers = this.listPlayers.length + 1;
+
+		var newRole = this.listRoles[Math.floor(Math.random()*this.listRoles.length)];
+
+		if(newRole.visible != true){
+			newRole =  this.setRandomRole();
+		}
+
+		if(newRole.roleName == 'Loup Garou'){
+			this.nbLoups += 1;
+
+			var maxLoups = Math.round(this.nbPlayers * this.listRoles[1].max); //Recup du nombre maxi de loups
+
+			if(this.nbLoups > maxLoups){
+				newRole =  this.setRandomRole();
+			}
+
+		} else if(newRole.roleName != 'Villageois'){ //Maxi 1 joueur des autres roles
+
+			var isAvailable = true;
+
+			this.listPlayers.forEach(function(player){
+				if(player.role == newRole.roleName)
+					isAvailable = false;
+			});
+
+			if(isAvailable != true) {
+				newRole =  this.setRandomRole();
+			}
+		}
+
+		return newRole;
+
 	}
 
 }
